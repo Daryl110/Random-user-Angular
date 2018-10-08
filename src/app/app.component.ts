6e2a35ac1b8593d81527c4faf368f989749acc72
Daryl110 <daryl.8220@gmail.com>
2018-10-07 19:57:22 -0500

    Terminacion de random user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'prueba2';
+export class AppComponent implements OnInit {
+
+  API_URL = 'https://randomuser.me/api/?results=50';
+  users;
+
+  constructor (private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get(this.API_URL).subscribe((data) => {
+      this.users = data;
+      this.users = this.users.results;
+      console.log(this.users);
+    });
+  }
 }
